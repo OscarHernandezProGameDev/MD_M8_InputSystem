36e27bd7521bd1f8f21dc7bf72bc39c9372e0a71
bugbit <ohernandezba@gmail.com>
2024-04-30 22:46:29 +0200

    Ejemplo movimiento 1d

--- Assets/Scripts/EmbeddedMovement1D.cs
+++ Assets/Scripts/EmbeddedMovement1D.cs
@@ -7,6 +7,8 @@ public class EmbeddedMovement1D : MonoBehaviour
 {
     public InputAction moveAction;
     public InputAction jumpAction;
+    public InputAction activateAction;
+    public InputAction testAction;
 
     [SerializeField] private float speed, jumpForce;
     private Rigidbody2D rb;
@@ -15,10 +17,20 @@ public class EmbeddedMovement1D : MonoBehaviour
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform groundCheckPos;
 
+    private bool ableToActivateSomething;
+
     void OnEnable()
     {
         moveAction.Enable();
         jumpAction.Enable();
+
+        activateAction.started += Activate;
+        activateAction.Enable();
+
+        // testAction.started += c => Debug.Log("test started");
+        // testAction.performed += c => Debug.Log("test perform");
+        // testAction.canceled += c => Debug.Log("test canceled");
+        // testAction.Enable();
     }
 
     void Start()
@@ -30,7 +42,7 @@ public class EmbeddedMovement1D : MonoBehaviour
     {
         direction = moveAction.ReadValue<float>();
         jumpValue = jumpAction.ReadValue<float>();
-        Debug.Log($"El valor del salto es: {jumpValue}");
+        //Debug.Log($"El valor del salto es: {jumpValue}");
 
         Flip();
     }
@@ -39,6 +51,12 @@ public class EmbeddedMovement1D : MonoBehaviour
     {
         rb.velocity = new Vector2(direction * speed, 0);
         Jump();
+    }
+
+    private void Activate(InputAction.CallbackContext context)
+    {
+        if (ableToActivateSomething)
+            Debug.Log("Fiesta de objetos");
     }
 
     private void Flip()
@@ -57,6 +75,22 @@ public class EmbeddedMovement1D : MonoBehaviour
 
     public bool IsGrounded() => Physics2D.Raycast(groundCheckPos.position, Vector2.down, 0.1f, groundLayer);
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Activable"))
+        {
+            ableToActivateSomething = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Activable"))
+        {
+            ableToActivateSomething = false;
+        }
+    }
+
     // Es mejor ordenar estos eventos según el orden de ejecución de unity
     // Lo podemos al final porque segun el execution order de unity es de lo úitimo que se ejecuta
 
@@ -64,5 +98,10 @@ public class EmbeddedMovement1D : MonoBehaviour
     {
         moveAction.Disable();
         jumpAction.Disable();
+
+        activateAction.started -= Activate;
+        activateAction.Disable();
+
+        //testAction.Disable();
     }
 }
